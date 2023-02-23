8bab8241d5ae8336a97ed6b8c044546ab1122909
HelloWorld7894 <danpojhan@protonmail.com>
2023-02-23 17:55:59 +0100

    update lol

--- src/main.py
+++ src/main.py
@@ -5,52 +5,65 @@ import time
 import math
 
 cap = cv2.VideoCapture(0)
-if not cap.isOpened():
-    print("Cannot open camera")
-    exit()
+
+def Thresh(img):
+    thresh = 10
+    img[img > thresh] = 255
+    img[img <= thresh] = 0
+
+    return img
+
+def HoughLines(img_in, img_out, mode):
+    if mode == "std":
+        lines = cv2.HoughLines(img_in, 1, np.pi / 180, 150, None, 0, 0)
+
+        if lines is not None:
+            for i in range(0, len(lines)):
+                rho = lines[i][0][0]
+                theta = lines[i][0][1]
+                a = math.cos(theta)
+                b = math.sin(theta)
+                x0 = a * rho
+                y0 = b * rho
+                pt1 = (int(x0 + 1000*(-b)), int(y0 + 1000*(a)))
+                pt2 = (int(x0 - 1000*(-b)), int(y0 - 1000*(a)))
+                cv2.line(img_out, pt1, pt2, (0,0,255), 3, cv2.LINE_AA)
+
+    elif mode == "p":
+        linesP = cv2.HoughLinesP(img_in,rho = 1,theta = 1*np.pi/180,threshold = 100,minLineLength = 100,maxLineGap = 50)
+    
+        if linesP is not None:
+            for i in range(0, len(linesP)):
+                l = linesP[i][0]
+                cv2.line(img_out, (l[0], l[1]), (l[2], l[3]), (0,0,255), 3, cv2.LINE_AA)
+
 
 
 while True:
     # Capture frame-by-frame
     ret, img = cap.read()
-    # if frame is read correctly ret is True
-    if not ret:
-        print("Can't receive frame (stream end?). Exiting ...")
-        break
+    test = img.copy()
 
+    img = cv2.GaussianBlur(img, (3, 3), 0)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    #dst = cv2.Canny(hpf, 30, 150, None, 3)
+    #print(dst.dtype)
+    grad_x = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=3, borderType=cv2.BORDER_DEFAULT)
+    grad_y = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=3, borderType=cv2.BORDER_DEFAULT)
+    
+    abs_grad_x = cv2.convertScaleAbs(grad_x)
+    abs_grad_y = cv2.convertScaleAbs(grad_y)
+    
+    dst = cv2.addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0)
+    dst = Thresh(dst)
+    #dst = cv2.blur(dst,(5,5))
+    #dst = Thresh(dst)
 
-
-    dst = cv2.Canny(img, 30, 150, None, 3)
-    
-    # Copy edges to the images that will display the results in BGR
-    cdst = cv2.cvtColor(dst, cv2.COLOR_GRAY2BGR)
-    cdstP = np.copy(cdst)
-    
-    lines = cv2.HoughLines(dst, 1, np.pi / 180, 150, None, 50, 10)
-    
-    if lines is not None:
-        for i in range(0, len(lines)):
-            rho = lines[i][0][0]
-            theta = lines[i][0][1]
-            a = math.cos(theta)
-            b = math.sin(theta)
-            x0 = a * rho
-            y0 = b * rho
-            pt1 = (int(x0 + 1000*(-b)), int(y0 + 1000*(a)))
-            pt2 = (int(x0 - 1000*(-b)), int(y0 - 1000*(a)))
-            cv2.line(cdst, pt1, pt2, (0,0,255), 3, cv2.LINE_AA)
-    
-    
-    linesP = cv2.HoughLinesP(dst, 1, np.pi / 180, 50, None, 50, 10)
-    
-    if linesP is not None:
-        for i in range(0, len(linesP)):
-            l = linesP[i][0]
-            cv2.line(cdstP, (l[0], l[1]), (l[2], l[3]), (0,0,255), 3, cv2.LINE_AA)
-    
+    HoughLines(dst, test, "p")
+            
     cv2.imshow("Source", img)
-    cv2.imshow("Detected Lines (in red) - Standard Hough Line Transform", cdst)
-    cv2.imshow("Detected Lines (in red) - Probabilistic Line Transform", cdstP)
+    cv2.imshow("Detected Lines (in red) - Probabilistic Line Transform", test)
+    cv2.imshow("sobel", dst)
 
 
 
